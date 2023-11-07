9855a575480d0cf39c890103341dd74ae60aa2af
Mark <lazutov.mark@mail.ru>
2023-11-07 15:49:45 +0300

    task3 commit

---
+++ task3/src/es6.spec.js
@@ -0,0 +1,52 @@
+const assert = require('assert');
+const core = require('./es6');
+
+describe('es6', () => {
+    describe('#fioToName', () => {
+        it('ФИО в Имя Фамилия корректно', () => {
+            assert.strictEqual(core.fioToName('Иванов Иван Иванович'), 'Иван Иванов');
+        });
+
+        it('ФИ в Имя Фамилия', () => {
+            assert.strictEqual(core.fioToName('Петров Петр'), 'Петр Петров');
+        });
+    });
+
+    describe('#filterUnique', () => {
+        it('массив с уникальными равен сам себе', () => {
+            assert.deepStrictEqual(core.filterUnique([1, 2, 3]), [1, 2, 3]);
+        });
+
+        it('массив с неуникальными отфильтрован', () => {
+            assert.deepStrictEqual(core.filterUnique([1, 1, 1, 1]), [1]);
+        });
+
+        it('пустой массив', () => {
+            assert.deepStrictEqual(core.filterUnique([]), []);
+        });
+    });
+
+    describe('#calculateSalaryDifference', () => {
+        it('считает разницу корректно', () => {
+            assert.strictEqual(core.calculateSalaryDifference([1, 2, 3]), 3);
+        });
+
+        it('на пустой массив возвращается falsy значение', () => {
+            assert.strictEqual(!!core.calculateSalaryDifference([]), false);
+        });
+    });
+
+    describe('#Dictionary', () => {
+        it('экземпляр класса создается', () => {
+            const dic = new core.Dictionary();
+
+            // Проверка аргументов
+            assert.strictEqual(dic.set(1, "abacaba"), null);
+            assert.strictEqual(dic.set("abacaba", "abababa"), undefined);
+
+            //Геттер по ключу
+            assert.strictEqual(dic.get("abacaba"), "abababa");
+            assert.strictEqual(dic.get("a"), undefined);
+        });
+    });
+});
